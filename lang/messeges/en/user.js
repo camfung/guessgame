4fbeec726cdae565dcd3c8f8ceff614ff2bcaba6
camfung <cameronfuung@gmail.com>
2024-09-06 12:47:38 -0700

    added comma

--- lang/messeges/en/user.js
+++ lang/messeges/en/user.js
@@ -2,6 +2,6 @@ strings = {
     typeSomethingHere: "Type something here",
     go: "Go",
     excelentMemory: "Excelent Memory",
-    wrongOrder: "Wrong Order"
+    wrongOrder: "Wrong Order", 
     gt7:"Cannot do more than 7"
 }
