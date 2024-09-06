2a49badde1a98539fbe7268e45fa1c0f694a72f9
camfung <cameronfuung@gmail.com>
2024-09-06 09:16:42 -0700

    first commit

---
+++ js/script.js
@@ -0,0 +1,191 @@
+let nodeHandler;
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function startGame(numberOfNodes) {
+    if (nodeHandler) {
+        nodeHandler.removeElements()
+    }
+    nodeHandler = new NodeHandler(numberOfNodes)
+    input.style.display = "none"
+    button.style.display = "none"
+    setTimeout(() => {
+
+        for (let node of nodeHandler.nodes) {
+            node.setPos()
+        }
+        for (let i = 0; i < numberOfNodes; i++) {
+            setTimeout(() => {
+                console.log("another change")
+                for (let node of nodeHandler.nodes) {
+                    node.setRandomPos()
+                }
+            }, 1000);
+        }
+
+
+        nodeHandler.hideNumbers()
+    }, numberOfNodes * 1000);
+}
+
+// created by gpt
+// prompt: make me a function that takes a x and y position and creates a div with the class box in the dom. 
+function createNodeDivAtPosition(x, y, innerHTML, ordered_number, parentId) {
+    // Create a new div element
+    const box = document.createElement('div');
+
+    box.className = 'box';
+    box.id = `node-${ordered_number}`
+
+    box.style.left = `${x}px`;
+    box.style.top = `${y}px`;
+    box.innerHTML = innerHTML
+    box.style.backgroundColor = getRandomColor()
+
+
+    let parent = document.getElementById(parentId)
+    parent.appendChild(box)
+    return box
+}
+
+class Node {
+    constructor(ordered_number, nodeHandler, parentId) {
+        let innerHTML = `<div> ${ordered_number} </div> `
+        this.ordered_number = ordered_number
+        this.domElement = createNodeDivAtPosition(0, 0, innerHTML, ordered_number, parentId)
+        this.nodeHandler = nodeHandler
+
+        let [x, y] = this.getRandomPosInWindow()
+        this.x = x
+        this.y = y
+        console.log("🚀 ~ Node ~ constructor ~ x:", x)
+        this.domElement.addEventListener("click", () => {
+            this.nodeHandler.nodeClicked(this.ordered_number)
+        })
+
+        if (ordered_number === undefined) {
+            throw Error("No ordered_number included")
+        }
+    }
+    setPos() {
+        this.domElement.style.position = "absolute"
+        this.domElement.style.left = this.x + "px"
+        this.domElement.style.top = this.y + "px"
+    }
+    // This method would be responsible for removing the node from the DOM
+    removeFromDOM() {
+        this.domElement.remove();
+    }
+
+    hideNumber() {
+        this.domElement.innerHTML = ""
+    }
+
+    showNumber() {
+        this.domElement.innerHTML = `${this.ordered_number}`
+    }
+    getRandomPosInWindow() {
+        let elementHeight = this.domElement.offsetHeight
+        let elementWidth = this.domElement.offsetWidth
+
+        let x = Math.floor(Math.random() * window.innerWidth);
+        let y = Math.floor(Math.random() * window.innerHeight);
+
+        while (this.x < 0 || this.y < 0 || this.x > window.innerWidth - elementWidth || this.y > window.innerHeight - elementHeight) {
+            x = Math.floor(Math.random() * window.innerWidth);
+            y = Math.floor(Math.random() * window.innerHeight);
+        }
+        return [x, y]
+    }
+    setRandomPos() {
+        let [x, y] = this.getRandomPosInWindow();
+        this.x = x
+        this.y = y
+        this.setPos()
+    }
+}
+
+class NodeHandler {
+    constructor(number_of_nodes) {
+        this.number_of_nodes = number_of_nodes || 4;
+        this.nodes = [];
+        this.gameStarted = false
+        this.nextIndexToClick = 1
+        for (let i = 1; i <= this.number_of_nodes; i++) {
+            this.nodes.push(new Node(i, this, "boxes"));
+        }
+    }
+    removeElements() {
+        for (let node of this.nodes) {
+            node.removeFromDOM()
+        }
+    }
+    nodeClicked(ordered_number) {
+        if (ordered_number == this.nextIndexToClick && this.gameStarted) {
+            this.nextIndexToClick++;
+            if (this.nextIndexToClick == this.number_of_nodes + 1) {
+                this.showNumbers()
+                alert(strings.excelentMemory)
+                input.style.display = "inline"
+                button.style.display = "inline"
+                this.gameStarted = false
+            }
+        }
+        else {
+            this.showNumbers()
+            alert(strings.wrongOrder)
+            this.gameStarted = false
+            input.style.display = "inline"
+            button.style.display = "inline"
+        }
+    }
+    updatePositions() {
+        for (let node of this.nodes) {
+            node.setPos()
+        }
+    }
+    showNumbers() {
+        for (let node of this.nodes) {
+            node.showNumber()
+        }
+    }
+    hideNumbers() {
+        for (let node of this.nodes) {
+            node.hideNumber()
+        }
+    }
+}
+
+
+
+// Create input and button elements
+const input = document.createElement('input');
+const button = document.createElement('button');
+
+// Set attributes and text for the button
+input.type = 'number';
+input.placeholder = strings.typeSomethingHere;
+button.textContent = strings.go;
+
+// Append elements to the div with id 'menu'
+const menuDiv = document.getElementById('menu');
+console.log("🚀 ~ menuDiv:", menuDiv)
+
+menuDiv.appendChild(input);
+menuDiv.appendChild(button);
+
+// Function to handle button click
+function handleButtonClick() {
+    startGame(parseInt(input.value))
+    nodeHandler.gameStarted = true
+}
+
+
+// Add event listeners
+button.addEventListener('click', handleButtonClick);
---
+++ lang/messeges/en/user.js
@@ -0,0 +1,6 @@
+strings = {
+    typeSomethingHere: "Type something here",
+    go: "Go",
+    excelentMemory: "Excelent Memory",
+    wrongOrder: "Wrong Order"
+}
